bfce26893ee711f453c5f40daed6260c38e21c1e
Oleksii Aleshchenko <oleksii.aleshchenko@netcracker.com>
2019-02-15 13:51:12 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int a = 1;
+        int b = 2;
+        System.out.println(a + " " + b);
+        System.out.printf("%d %d", a, b);
+
+        for (int i = 0; i < 1; i++) {
+            
+        }
+
+        for (int i = 0; i < 2; i++) {
+            
+        }
+    }
+}
+
+//begin
+//end.
